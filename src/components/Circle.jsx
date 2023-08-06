6eca3ab7d50126379c3c6e6433037f464bc98b71
Timi <tunmisebabade@gmail.com>
2023-08-06 19:18:53 +0100

    second commit

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -2,28 +2,34 @@
 export default function Circle(){
     return (
         <div className="p-5 lg:10 grid grid-cols-4 gap-5 lg:gap-10 rounded-3xl shadow-xl w-fit mx-auto">
-            <Blue />
-            <Red />
-            <Yellow />
-            <Blue />
-            <Red />
-            <Yellow />
+            <ALL color="blue"/>
+            <ALL color="red"/>
+            <ALL color="yellow"/>
+            <ALL color="blue"/>
+            <ALL color="red"/>
+            <ALL color="yellow"/>
         </div>
     )
 }
 
-function Blue(){
+function ALL(props) {
     return (
-        <div className="w-16 lg:w-20 aspect-square rounded-full bg-blue-700"></div>
+        <div className="w-16 lg:w-20 aspect-square rounded-full" style={{backgroundColor: props.color}}></div>
     )
 }
-function Red(){
-    return (
-        <div className="w-16 lg:w-20 aspect-square rounded-full bg-red-500"></div>
-    )
-}
-function Yellow(){
-    return (
-        <div className="w-16 lg:w-20 aspect-square rounded-full bg-yellow-500"></div>
-    )
-}
+
+// function Blue(){
+//     return (
+//         <div className="w-16 lg:w-20 aspect-square rounded-full bg-blue-700"></div>
+//     )
+// }
+// function Red(){
+//     return (
+//         <div className="w-16 lg:w-20 aspect-square rounded-full bg-red-500"></div>
+//     )
+// }
+// function Yellow(){
+//     return (
+//         <div className="w-16 lg:w-20 aspect-square rounded-full bg-yellow-500"></div>
+//     )
+// }
