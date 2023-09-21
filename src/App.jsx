53790618c0c5df53b7fc5924f9a889bde083d0fc
Timi <tunmisebabade@gmail.com>
2023-09-21 13:56:45 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import Circle from "./components/Circle";
 function App() {
   return (
-    <div className="flex justify-center flex-col  min-h-screen">
-      <h1 className="my-3 text-lg lg:text-4xl text-center font-bold">TIMILEHIN</h1>
-      <Circle />
+    <div className="">
+      <h1 className="text-lg lg:text-4xl text-center font-bold">LET'S BUILD A SEARCH ENGINE</h1>
     </div>
   );
 }
