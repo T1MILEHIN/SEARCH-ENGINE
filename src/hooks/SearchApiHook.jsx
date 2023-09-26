f2a5bbd39db79701c6162e2b1e27215285cc0650
Timi <tunmisebabade@gmail.com>
2023-09-26 03:50:10 +0100

    first commit

--- src/hooks/SearchApiHook.jsx
+++ src/hooks/SearchApiHook.jsx
@@ -2,8 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const SearchApiHook = (searchValue)=> {
-    const APIkey = "AIzaSyB2pqTADxjHb5quJPCxOxMjOA0oOlXDqjM";
+    const APIkey = "AIzaSyDkFGe_A7F0nRK8kWe2j2B74vG_x59nnPA";
     // working AIzaSyDkFGe_A7F0nRK8kWe2j2B74vG_x59nnPA
+    // Not working AIzaSyB2pqTADxjHb5quJPCxOxMjOA0oOlXDqjM
     const engineID = "c7d1f89829903401e"
     const url = `https://www.googleapis.com/customsearch/v1?key=${APIkey}&cx=${engineID}&q=`;
     return useQuery({
