a2e1511862ab6abc9febe642ad8a6d9be701ac41
Timi <tunmisebabade@gmail.com>
2023-09-26 03:13:26 +0100

    first commit

--- src/components/SearchDisplay.jsx
+++ src/components/SearchDisplay.jsx
@@ -5,10 +5,10 @@ const SearchDisplay = ({searchValue ,cacheId, title, link, snippet, displayLink,
     return (
         <div key={cacheId} className="flex gap-4 md:w-[50%]">
             {searchValue.includes("img" || "image" || "images") && <LazyLoadImage effect="blur" src={pagemap?.cse_image[0]?.src} alt="" className="rounded-md w-36 aspect-square object-cover" />}
-            <div>
-                <a href={link} className="font-bold text-blue-400 underline underline-offset-2">{title}</a>
+            <div className='text-sm md:text-lg'>
+                <a href={link} className="font-black text-blue-400 underline underline-offset-2">{title}</a>
                 <p className="font-thin text-green-400">{displayLink}</p>
-                <p className="text-slate-300">{snippet}</p>
+                <p className="text-slate-100">{snippet}</p>
             </div>
         </div>
     )
