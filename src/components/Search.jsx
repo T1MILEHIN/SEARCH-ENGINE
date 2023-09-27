894af4588fffbad4af45f5d7a269582367eaae51
Timi <tunmisebabade@gmail.com>
2023-09-27 17:03:21 +0100

    first commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
         <div className="z-10 w-[95%] md:w-[60%] bg-blur md:p-5 p-2">
             <motion.h1 initial={{scale: 0.6, opacity: 0}} animate={{scale: 1, opacity: 1}} transition={{type: "spring", stiffness: 250}} className="text-2xl md:text-4xl text-center font-mono">T SEARCH</motion.h1>
             <HomeForm />
-            <p className="text-center text-sm md:text-lg font-thin">Terms & Conditions Applied</p>
+            <p className="text-center text-xs md:text-lg font-thin">Terms & Conditions Applied</p>
         </div>
     )
 }
