539cf0bb32ae41f7d848b7ab0eab1511f030376d
Timi <tunmisebabade@gmail.com>
2023-11-17 08:20:24 +0100

    corrections

--- src/components/SearchDisplay.jsx
+++ src/components/SearchDisplay.jsx
@@ -7,7 +7,7 @@ const SearchDisplay = ({searchValue ,cacheId, title, link, snippet, displayLink,
             {searchValue.includes("img" || "image" || "images") && <LazyLoadImage effect="blur" src={pagemap?.cse_image[0]?.src} alt="" className="rounded-md w-36 aspect-square object-cover" />}
             <div className='text-sm md:text-lg'>
                 <a href={link} className="font-black text-blue-400 underline underline-offset-2">{title}</a>
-                <p className="font-thin text-green-400">{displayLink}</p>
+                <p className="font-thin text-green-400 underline">{displayLink}</p>
                 <p className="text-black">{snippet}</p>
             </div>
         </div>
